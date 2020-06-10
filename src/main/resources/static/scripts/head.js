8da68bcd4ec27ebcfd632c61f9559396f3f5ea3d
zhuk0513 <938282597@qq.com>
2020-06-10 08:45:29 +0800

    头文件引入

---
+++ src/main/resources/static/scripts/head.js
@@ -0,0 +1,10 @@
+// document.write('<base th:href="@{/}">');
+document.write('<link rel="stylesheet" href="scripts/element-ui/lib/theme-chalk/index.css">');
+document.write('<link rel="stylesheet" href="stylesheet/css/main.css">');
+
+document.write('<script src="scripts/jquery.min.js"></script>');
+document.write('<script src="scripts/vue/vue.js"></script>');
+document.write('<script src="scripts/element-ui/lib/index.js"></script>');
+document.write('<script src="scripts/ajax.js"></script>');
+document.write('<script src="scripts/myUtils.js"></script>');
+
